5eea085c7d9dd5a79f5c81a0730737bb0cce8a34
Eric Bastarache <eric_bastarache@hotmail.com>
2017-10-15 00:02:35 -0400

    very preliminary addition of login container

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import LoginContainer from './containers/LoginContainer';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header headerText="Hello World"/>
+        <LoginContainer />
         <Footer footerText="Lorem ipsum dolor amet" />
       </div>
     );
