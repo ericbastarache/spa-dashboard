c1f605a19226897feb488b1a7ef0691b98d078e2
Eric Bastarache <eric_bastarache@hotmail.com>
2017-10-14 23:27:46 -0400

    created login container

---
+++ src/containers/LoginContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Login from '../components/Login/Login';
+class LoginContainer extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Login username="" password=""/>
+        );
+    }
+}
