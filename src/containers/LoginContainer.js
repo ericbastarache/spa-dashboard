4318a7397d4f00070e685c62f0f8ccc1dd58486b
Eric Bastarache <eric_bastarache@hotmail.com>
2017-10-21 17:35:19 -0400

    removed login component (unnecessary)

--- src/containers/LoginContainer.js
+++ src/containers/LoginContainer.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
-import Login from '../components/Login/Login';
 class LoginContainer extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            username: '',
+            password: ''
+        };
+    }
+
+    handleChange (e) {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
     }
 
     render() {
         return (
-            <Login username="" password=""/>
+            
         );
     }
 }
 
-export default LoginContainer;
+const mapStateToProps = () => {
+    return {
+
+    };
+};
+
+export default connect(mapStateToProps)(LoginContainer);
