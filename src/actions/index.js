f545c9f2b14ac7982f04348a116190b276d2e404
Eric Bastarache <eric_bastarache@hotmail.com>
2017-10-22 14:51:41 -0400

    updated register action

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,8 +3,13 @@ import { USER_REGISTER, USER_LOGIN } from './types';
 const API_URL = 'http://localhost:3001/api/v1';
 
 export const createUser = (credentials) => {
+    let headers = new Headers({
+        'Access-Control-Allow-Origin': '*'
+    });
     const request = fetch(`${API_URL}/register`, {
-
+        method: 'POST',
+        headers,
+        mode: 'cors'
     });
     return {
         type: USER_REGISTER,
