55e028533cfa1c3f502646bfc96ed6633bc471fc
Eric Bastarache <eric_bastarache@hotmail.com>
2017-10-22 14:34:06 -0400

    added test login route

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,18 @@
 const Router = require('koa-router');
 const router = new Router();
 
-router.get('/', (ctx, next) => {
+router.get('/api/v1', (ctx, next) => {
     ctx.body = "Hello World";
 })
+
+router.get('/api/v1/login', (ctx, next) => {
+    console.log(ctx, next, this);
+    ctx.body = {
+        user: {
+            username: 'Eric',
+            password: 'abcdefghijklmnopqrstuvwxyz'
+        }
+    }
+});
 
 module.exports = router;
